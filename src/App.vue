3a872cae88075f91c201487feb4864ed29accda8
Jesús Romero Estrada <jesus.rom97@gmail.com>
2023-04-10 16:54:01 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <Calculator/>
+    <List/>
   </div>
 </template>
 
 <script>
-import Calculator from './components/Calculator.vue'
-
+import List from './components/list.vue'
 export default {
   name: 'App',
   components: {
-    Calculator
+    List
   }
 }
 </script>
 
 <style>
- body {
-    background: #31475e !important;
-  }
-  .button {
-  background-color: #4CAF50;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
+  #app {
+    font-family: Roboto, sans-serif;
+    text-align: center;
+    padding: 20px;
+    background-color: beige;
   }
 </style>
